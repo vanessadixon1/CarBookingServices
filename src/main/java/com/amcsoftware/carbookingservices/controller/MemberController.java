7f172309144fa911cb4e8864c3d93e973f717941
vanessadixon1 <mclendonfamily88@gmail.com>
2023-06-26 13:24:20 -0400

    refactored code for MemberController and MemberService

--- src/main/java/com/amcsoftware/carbookingservices/controller/MemberController.java
+++ src/main/java/com/amcsoftware/carbookingservices/controller/MemberController.java
@@ -12,9 +12,11 @@ import java.util.List;
 @RestController
 @RequestMapping("api/v1/member")
 public class MemberController {
+
     private final MemberService memberService;
 
     public MemberController(MemberService memberService) {
+
         this.memberService = memberService;
     }
 
@@ -32,11 +34,13 @@ public class MemberController {
 
     @GetMapping()
     public List<Member> getMembers() {
+
         return memberService.getMembers();
     }
 
     @GetMapping("/")
     public Member getMembers(@RequestParam(value = "lastname") String lastName, @RequestParam(value = "email") String email) {
+
         return memberService.findLastNameAndEmail(lastName, email);
     }
 
@@ -47,17 +51,25 @@ public class MemberController {
 
     @DeleteMapping("/delete/{email}")
     public ResponseEntity<CustomResponse> deleteMember(@PathVariable("email") String email) {
+
         memberService.deleteMember(email);
+
         CustomResponse response = new CustomResponse();
-        response.setMessage("Member with email address:  " + email  + " has been deleted successfully");
+
+        response.setMessage("member with email address: " + email  + " has been deleted successfully");
+
         return ResponseEntity.status(HttpStatus.OK).body(response);
     }
 
     @PutMapping("/update/{email}")
     public ResponseEntity<CustomResponse> updateMember(@PathVariable("email") String email, @RequestBody Member member) {
+
         memberService.updateMember(email, member);
+
         CustomResponse response = new CustomResponse();
+
         response.setMessage("updated successfully");
+
         return ResponseEntity.status(HttpStatus.OK).body(response);
     }
 
--- src/main/java/com/amcsoftware/carbookingservices/service/MemberService.java
+++ src/main/java/com/amcsoftware/carbookingservices/service/MemberService.java
@@ -1,6 +1,7 @@
 package com.amcsoftware.carbookingservices.service;
 
 import com.amcsoftware.carbookingservices.dao.MemberDao;
+import com.amcsoftware.carbookingservices.exceptions.BadRequestException;
 import com.amcsoftware.carbookingservices.exceptions.DuplicateResourceException;
 import com.amcsoftware.carbookingservices.exceptions.ResourceNotFoundException;
 import com.amcsoftware.carbookingservices.model.Member;
@@ -25,6 +26,8 @@ public class MemberService {
 
     public void userRegistrationInfo(Member member) {
 
+        validateValuesWerePassed(member);
+
         long ageValidated = YEARS.between(member.getDateOfBirth(), LocalDate.now());
 
         boolean isPhoneNumberValid = Pattern.compile("^(\\d{3}[-]){2}\\d{4}$").matcher(member.getPhoneNumber()).matches();
@@ -43,140 +46,185 @@ public class MemberService {
             throw new IllegalArgumentException("registration unsuccessful - must be 21 or older");
         }
 
-        if(!memberDao.existsByEmail(member.getEmail())) {
-            member.setFirstName(member.getFirstName().substring(0,1).toUpperCase() +
-                    member.getFirstName().substring(1));
-
-            memberDao.saveMember(member);
-
-        } else {
-            throw new DuplicateResourceException("member [%s]".formatted(member.getEmail()) + " already exist");
+        if(memberDao.existsByEmail(member.getEmail())) {
+            throw new DuplicateResourceException("a member with the email %s".formatted(member.getEmail()) + " already exist");
         }
+
+        member.setFirstName(member.getFirstName().substring(0,1).toUpperCase() +
+                member.getFirstName().substring(1));
+
+        memberDao.saveMember(member);
     }
 
     public List<Member> getMembers() {
 
         if(memberDao.memberCount() == 0) {
-            throw new NullPointerException("no members available");
-        }else {
-            return memberDao.getAllMembers();
+            throw new ResourceNotFoundException("no data available");
         }
+
+        return memberDao.getAllMembers();
+
     }
 
     public Member findLastNameAndEmail(String lastName, String email) {
+
+        if(lastName.length() == 0 || email.length() == 0) {
+            throw new BadRequestException("the last name or email is not valid");
+        }
+
         boolean isEmailValid = Pattern.compile("[a-zA-Z0-9]{1,}[@]{1}[a-z]{5,}[.]{1}+[a-z]{3}").matcher(email).matches();
 
         String lastname = lastName.substring(0,1).toUpperCase() + lastName.substring(1);
 
         if(!isEmailValid) {
-            throw new IllegalArgumentException("email [%s]".formatted(email) + " is invalid" );
+            throw new IllegalArgumentException("email %s".formatted(email) + " is invalid" );
         }
-        if(memberDao.existsByLastNameAndEmail(lastname,email)) {
-            return memberDao.findMemberByLastNameAndEmail(lastname, email);
-        } else {
+
+        if(!memberDao.existsByLastNameAndEmail(lastname,email)) {
             throw new ResourceNotFoundException("the lastName %s".formatted(lastname) + " and email: %s ".formatted(email) + "doesn't exist");
         }
 
+        return memberDao.findMemberByLastNameAndEmail(lastname, email);
     }
 
     public Member findMemberWithEmail(String email) {
 
+        if(email.length() == 0) {
+            throw new BadRequestException("the last name or email is not valid");
+        }
+
         boolean isEmailValid = Pattern.compile("[a-zA-Z0-9]{1,}[@]{1}[a-z]{5,}[.]{1}+[a-z]{3}").matcher(email).matches();
 
         if(!isEmailValid) {
-            throw new IllegalArgumentException("email %s".formatted(email) + " is invalid" );
+            throw new IllegalArgumentException("email %s".formatted(email) + " is invalid");
         }
 
-        if(memberDao.existsByEmail(email)) {
-            return memberDao.findMemberByEmail(email);
-        }else {
+        if(!memberDao.existsByEmail(email)) {
             throw new ResourceNotFoundException("the email %s".formatted(email) + " doesn't exist");
         }
+
+        return memberDao.findMemberByEmail(email);
     }
 
     public Member findMemberById(UUID userId) {
 
         return memberDao.findById(userId)
-                .orElseThrow(() -> new ResourceNotFoundException("uer [%s] ".formatted(userId) + " was not found") );
+                .orElseThrow(() -> new ResourceNotFoundException("uer %s".formatted(userId) + " was not found") );
     }
 
     public void deleteMember(String email) {
 
+        if(!memberDao.existsByEmail(email)) {
+            throw new ResourceNotFoundException("the email %s".formatted(email) + " doesn't exist");
+        }
+
         boolean isEmailValid = Pattern.compile("[a-zA-Z0-9]{1,}[@]{1}[a-z]{5,}[.]{1}+[a-z]{3}").matcher(email).matches();
 
         if(!isEmailValid) {
-            throw new IllegalArgumentException("email %s".formatted(email) + " is invalid" );
+            throw new BadRequestException("email %s".formatted(email) + " is invalid");
         }
 
         Member locatedMember = memberDao.findMemberByEmail(email);
 
-        if(locatedMember.getReservations().size() != 0 ) {
-            throw new IllegalArgumentException("unsuccessful deletion - user has " + locatedMember.getReservations().size() + " reservation booked" );
+        if(locatedMember.getReservations().size() != 0) {
+            throw new BadRequestException("unsuccessful deletion - remove the current reservation(s) and then remove user." );
         }
 
         memberDao.deleteMember(locatedMember);
     }
 
     public void updateMember(String email, Member member) {
+
+        if(!memberDao.existsByEmail(email)) {
+            throw new BadRequestException("unable to update - no user with the email %s".formatted(email) + " was found");
+        }
+
         boolean isUpdatingEmailValid = Pattern.compile("[a-zA-Z0-9]{1,}[@]{1}[a-z]{5,}[.]{1}+[a-z]{3}").matcher(member.getEmail()).matches();
 
         boolean isEmailValid = Pattern.compile("[a-zA-Z0-9]{1,}[@]{1}[a-z]{5,}[.]{1}+[a-z]{3}").matcher(email).matches();
 
+        boolean changed = false;
+
         if(!isEmailValid || !isUpdatingEmailValid) {
-            throw new IllegalArgumentException("email %s" + " is invalid" );
+            throw new BadRequestException("email is invalid" );
         }
 
         if(!memberDao.existsByEmail(email)) {
             throw new ResourceNotFoundException("no user with email %s".formatted(email) + " found");
         }
+
         Member locatedMember = memberDao.findMemberByEmail(email);
-        System.out.println(locatedMember.getFirstName());
 
-        if(!locatedMember.getFirstName().equals(member.getEmail())) {
+        if(!member.getFirstName().equals("") && !locatedMember.getFirstName().equals(member.getEmail())) {
             locatedMember.setFirstName(member.getFirstName());
+            changed = true;
         }
 
-        if(!locatedMember.getLastName().equals(member.getLastName())) {
+        if(!member.getLastName().equals("") && !locatedMember.getLastName().equals(member.getLastName())) {
             locatedMember.setLastName(member.getLastName());
+            changed = true;
         }
 
-        if(locatedMember.getMiddleInitial() == null || !locatedMember.getMiddleInitial().equals(member.getMiddleInitial())) {
+        if(!locatedMember.getMiddleInitial().equals(member.getMiddleInitial())) {
             locatedMember.setMiddleInitial(member.getMiddleInitial());
+            changed = true;
         }
 
-        if(!locatedMember.getPhoneNumber().equals(member.getPhoneNumber())) {
+        if(!member.getPhoneNumber().equals("") && !locatedMember.getPhoneNumber().equals(member.getPhoneNumber())) {
             locatedMember.setPhoneNumber(member.getPhoneNumber());
+            changed = true;
         }
 
-        if(!locatedMember.getEmail().equals(member.getEmail())) {
+        if(!member.getEmail().equals("") && !locatedMember.getEmail().equals(member.getEmail())) {
             locatedMember.setEmail(member.getEmail());
+            changed = true;
         }
 
-        if(!locatedMember.getDateOfBirth().equals(member.getDateOfBirth())) {
+        if(member.getDateOfBirth() != null && !locatedMember.getDateOfBirth().equals(member.getDateOfBirth())) {
             locatedMember.setDateOfBirth(member.getDateOfBirth());
+            changed = true;
         }
 
-        if(locatedMember.getStreetNo() != member.getStreetNo()) {
+        if(member.getStreetNo() != 0 && locatedMember.getStreetNo() != member.getStreetNo()) {
             locatedMember.setStreetNo(member.getStreetNo());
+            changed = true;
         }
 
-        if(!locatedMember.getStreetAddress().equals(member.getStreetAddress())) {
+        if(!member.getStreetAddress().equals("") && !locatedMember.getStreetAddress().equals(member.getStreetAddress())) {
             locatedMember.setStreetAddress(member.getStreetAddress());
+            changed = true;
         }
 
-        if(!locatedMember.getCity().equals(member.getCity())) {
+        if(!member.getCity().equals("") && !locatedMember.getCity().equals(member.getCity())) {
             locatedMember.setCity(member.getCity());
+            changed = true;
         }
 
-        if(locatedMember.getZipCode() != member.getZipCode()) {
+        if(member.getZipCode() != 0 && locatedMember.getZipCode() != member.getZipCode()) {
             locatedMember.setZipCode(member.getZipCode());
+            changed = true;
         }
 
-        if(!locatedMember.getState().equals(member.getState())) {
+        if(!member.getState().equals("") && !locatedMember.getState().equals(member.getState())) {
             locatedMember.setState(member.getState());
+            changed = true;
+        }
+
+        if(!changed) {
+            throw new BadRequestException("no changes were made");
         }
 
         memberDao.saveMember(locatedMember);
+    }
+
+    private void validateValuesWerePassed(Member member) {
+        if(member.getFirstName().equals("") || member.getLastName().equals("") || member.getPhoneNumber().equals("") ||
+                member.getEmail().equals("") || member.getDateOfBirth().equals("") || member.getStreetNo() != 0 ||
+                member.getStreetAddress().equals("") || member.getCity().equals("") || member.getState().equals("") ||
+                member.getZipCode() != 0) {
+
+            throw new BadRequestException("invalid values sent through request");
+        }
     }
 
 }
